4e78bab7daaab73259bd10fa87c3368213d966a8
olle1134 <99058610+olle1134@users.noreply.github.com>
2023-08-31 15:54:42 +1200

    Add tests for fingerprint authentication feature
    - Add unit tests to verify the fingerprint authentication feature.
    - Test the initial rendering of the "Authenticate with Fingerprint" button.
    - Confirm that `isAuthenticated` becomes true after successful authentication.
    - Validate that `isAuthenticated` remains false on failed authentication.
    
    These tests cover the fingerprint authentication scenarios, ensuring the accuracy of the feature's functionality.

--- tests/App.test.js
+++ tests/App.test.js
@@ -1,22 +1,56 @@
-import { render, fireEvent } from "@testing-library/react-native";
-import App from "../App";
+import React from "react";
+import { render, fireEvent, act } from "@testing-library/react-native";
+import App from "../App"; // Update this import path as needed
 
-jest.mock("@react-native-async-storage/async-storage", () => ({
-  getItem: jest.fn(),
-  setItem: jest.fn(),
+// Mock the expo-local-authentication module
+jest.mock("expo-local-authentication", () => ({
+  authenticateAsync: jest.fn(),
 }));
-test("addTask adds task with selected date", () => {
-  const { getByText, getByPlaceholderText } = render(<App />);
 
-  const taskNameInput = getByPlaceholderText("Enter task name...");
-  const addButton = getByText("Add");
+describe("Fingerprint Authentication", () => {
+  it("renders the authentication button initially", () => {
+    const { getByText } = render(<App />);
+    const authButton = getByText("Authenticate with Fingerprint");
+    expect(authButton).toBeTruthy();
+  });
 
-  fireEvent.changeText(taskNameInput, "Test Task");
-  fireEvent.press(addButton);
+  it("sets isAuthenticated to true on successful authentication", async () => {
+    // Mock a successful authentication
+    const mockSuccess = { success: true };
+    require("expo-local-authentication").authenticateAsync.mockResolvedValue(
+      mockSuccess
+    );
 
-  const mockAddTask = jest.fn();
-  const mockSelectedDateTime = "2023-08-27T15:30:00";
-  mockAddTask("Test Task", mockSelectedDateTime);
+    const { getByText, findByText, queryByText } = render(<App />);
+    const authButton = getByText("Authenticate with Fingerprint");
 
-  expect(mockAddTask).toHaveBeenCalledWith("Test Task", mockSelectedDateTime);
+    await act(async () => {
+      fireEvent.press(authButton);
+      // Wait for authentication to complete
+      await findByText("Reminders"); // Assuming this text is rendered after authentication
+    });
+
+    await act(async () => {
+      // Check if the authentication button is still in the DOM
+      const updatedAuthButton = queryByText("Authenticate with Fingerprint");
+      expect(updatedAuthButton).toBeNull(); // Button should not be rendered
+    });
+  });
+
+  it("does not change isAuthenticated on failed authentication", async () => {
+    // Mock a failed authentication
+    const mockFailure = { success: false };
+    require("expo-local-authentication").authenticateAsync.mockResolvedValue(
+      mockFailure
+    );
+
+    const { getByText, findByText } = render(<App />);
+    const authButton = getByText("Authenticate with Fingerprint");
+    fireEvent.press(authButton);
+
+    // Wait for authentication to complete
+    await findByText("Authenticate with Fingerprint"); // Button should still be rendered
+
+    expect(authButton).toBeTruthy(); // Button should be rendered
+  });
 });
