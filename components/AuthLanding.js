fb71b8f49d1e8c321223b3a3ee06386fe4384b48
olle1134 <99058610+olle1134@users.noreply.github.com>
2023-09-02 18:43:18 +1200

    Improved AuthLanding.js styling
    - Separated welcome and subtext for cleaner design.
    - Made subtext italic for a friendly touch.
    - Updated button text to "Unlock with Fingerprint."
    - Increased the size of the unlock button

--- components/AuthLanding.js
+++ components/AuthLanding.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
 
 const AuthLanding = ({ onAuthenticationSuccess, handleFingerprintAuth }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcomeText}>
-        Welcome to Your Reminders. Let's tackle your tasks one at a time!
-      </Text>
-      <Button
-        title="Authenticate with Fingerprint"
+      <View style={styles.textContainer}>
+        <Text style={styles.welcomeText}>Welcome to Your Reminders.</Text>
+        <Text style={styles.subText}>
+          Let's tackle your tasks one at a time!
+        </Text>
+      </View>
+      <TouchableOpacity
+        style={styles.authButton}
+        activeOpacity={1}
         onPress={() => {
           handleFingerprintAuth().then((success) => {
             if (success) {
@@ -16,7 +20,9 @@ const AuthLanding = ({ onAuthenticationSuccess, handleFingerprintAuth }) => {
             }
           });
         }}
-      />
+      >
+        <Text style={styles.buttonText}>Unlock with Fingerprint</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -26,13 +32,43 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "white",
+    backgroundColor: "#f9f9f9",
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
   welcomeText: {
     fontSize: 24,
-    fontWeight: "bold",
+    fontWeight: "600",
     textAlign: "center",
     color: "black",
+    marginBottom: 20,
+  },
+  subText: {
+    fontSize: 19,
+    fontWeight: "600",
+    textAlign: "center",
+    color: "black",
+    marginBottom: 10, // Add space between the lines
+    fontStyle: "italic",
+  },
+  buttonContainer: {
+    marginBottom: 40, // Adjust the margin to move the button up
+    fontSize: "24",
+  },
+  authButton: {
+    backgroundColor: "#3498DB", // Customize the button's background color
+    padding: 18, // Adjust padding to change button size
+    borderRadius: 8, // Add border radius for rounded corners
+    marginBottom: 55, // Adjust
+    elevation: 10,
+  },
+  buttonText: {
+    color: "white", // Customize the button text color
+    fontSize: 18, // Customize the button text size
+    fontWeight: "500",
   },
 });
 
